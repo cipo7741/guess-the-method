921a365543dad3e4b206c936062494455d2ba1f8
cipo7741 <a@b.c>
2020-02-18 22:29:49 +0100

    polished the code in preparation for package selection

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,32 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+### TODO
+
+- revise timeout of result message
+- switch packages
+    - drop down
+    - select included packages
+        - score per package?
+- session management (cookies?)
+    - no repetitions
+    - remember score on comeback
+
+## Changed
+
+- correct / false centered bellow
+    - red and green background  
+- new helper functions for package string management (for switching packages)
+
+## [0.0.3] - 2016-03-17
+
+### Changed
+
+- questionmark white
+- enhancement: question mark in the middle of the button
 - mvc:
   - model: game -> quests
+
 
 ## [0.0.2] - 2016-03-15
 
--- src/guess.js
+++ src/guess.js
@@ -1,36 +1,60 @@
 'use strict'
 
-var url = 'data/java/lang/Number/Double.json';
-var javaPackage = url.substring(5, url.length - 5).split('/').join('.')
+var currentPackageArray = ['java', 'lang', 'Number', 'Double'];
+var wrongResultColor = '#FFC4C4';
+var rightResultColor = '#BAFFBD';
+var resourcesPath = 'data';
+var resourcesExtension = '.json';
+
 var method, jsonData;
 var numPoints, numQuests, numTries;
 
-var loadJson = function(url){
-  return new Promise(function(resolve, reject) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', url, true);
-    xmlhttp.onload = function() {
-      if (xmlhttp.status === 200) {
-        numPoints = 0;
-        numQuests = 0;
-        jsonData = JSON.parse(xmlhttp.responseText);
-        newQuest(jsonData);
-      }
-    }
-    xmlhttp.onerror = function() {
-      Error('Data didn\'t load successfully; error code:' + request.statusText);
-    }
-    xmlhttp.send();
-  });
+// 'java/lang/Number/Double' -> ['data', 'java', 'lang', 'Number', 'Double']
+var getPackageArray = function(path) {
+    return path.split('/').join('.')
 }
 
-loadJson(url)
+// (['data', 'java', 'lang', 'Number', 'Double'], '/') -> 'data/java/lang/Number'
+var getBasePackageString = function(packageArray, seperator) {
+    return packageArray.splice(0,packageArray.length-1).join(seperator);
+}
+
+// ['data', 'java', 'lang', 'Number', 'Double'] -> 'Double'
+var getClassString = function(packageArray) {
+    return packageArray[packageArray.length-1]
+}
+
+
+var loadJson = function(packageArray){
+    return new Promise(
+        function(resolve, reject) {
+            var xmlhttp = new XMLHttpRequest();
+            var path = resourcesPath + '/' + getBasePackageString(packageArray, '/') + '/' + getClassString(packageArray) + resourcesExtension;
+            xmlhttp.open('GET', path, true);
+            xmlhttp.onload = function() {
+                if (xmlhttp.status === 200) {
+                    numPoints = 0;
+                    numQuests = 0;
+                    jsonData = JSON.parse(xmlhttp.responseText);
+                    newQuest(jsonData);
+                }
+            }
+            xmlhttp.onerror = function() {
+                Error('Data didn\'t load successfully; error code:' + request.statusText);
+            }
+            xmlhttp.send();
+        }
+        );
+}
+
+loadJson(currentPackageArray)
 
 var randomSeed = function(x) {
     return Math.floor((Math.random() * x));
 }
 
 var newQuest = function(arr) {
+    //document.getElementById('result').setAttribute('background', '#fff');
     numQuests += 1;
     var i = randomSeed(arr.length)
     buildQuest(arr, i);
@@ -49,53 +73,70 @@ function buildQuest(arr, i) {
     guessInput.setAttribute('class', 'focus');
     guessInput.setAttribute('maxlength', arr[i].name.length);
     guessInput.setAttribute('style', 'width: calc(15px *' + arr[i].name.length + ');');
-    guessInput = new XMLSerializer().serializeToString(guessInput)
-    document.getElementById('guess').innerHTML = arr[i].type + ' ' + javaPackage + '.'
-    document.getElementById('guess').innerHTML += guessInput + '(' + arr[i].args + ')';
+    var guessInputXml = new XMLSerializer().serializeToString(guessInput);
+        
+    // <select name='top5' size='5'>
+    //     <option>Heino</option>
+    //     <option>Michael Jackson</option>
+    //     <option>Tom Waits</option>
+    //     <option>Nina Hagen</option>
+    //     <option>Marianne Rosenberg</option>
+    //   </select>
+    var guessClassSelect = document.createElement('select');
+    guessClassSelect.setAttribute('name', 'classes');
+    guessClassSelect.setAttribute('size', '2');
+    var fs = require('fs');
+    var path = resourcesPath + '/' + getBasePackageString(currentPackageArray, '/') + '/' + getClassString(currentPackageArray) + resourcesExtension;
+    var files = fs.readdirSync(path);
+    for (var i = files.length - 1; i >= 0; i--) {
+        guessClassSelect.option = getClassString(files[i]);
+    }
+    var guessClassSelectXml = new XMLSerializer().serializeToString(guessClassSelect)
+
+    document.getElementById('guess').innerHTML = arr[i].type + ' ';
+    document.getElementById('guess').innerHTML += javaPackageString.replace(currentClass) + '.';
+    document.getElementById('guess').innerHTML += guessClassSelectXml
+    document.getElementById('guess').innerHTML += guessInputXml + '(' + arr[i].args + ')';
     document.querySelector('input.focus').focus();
 }
 
-var timeoutResult = function() {
-    setTimeout('blankResult()', 5000);
-}
-
 var blankResult = function() {
-    return document.getElementById('result').innerHTML = ' ';
+    document.getElementById('flag').innerHTML = ' ';
+    document.getElementById('flag').style.background = 'white';
 }
 
 var isCorrect = function() {
-    var x;
-    x = document.getElementById('guessInput').value;
-    if (x === method) {
-        return true;
-    } else {
-        document.getElementById('result').innerHTML = ' ';
-        return false;
-    }
+    return document.getElementById('guessInput').value === method;
 }
 
 var check = function() {
     var text;
+    var color = 'white';
+    var answer = '';
     if (isCorrect()) {
         text = 'Correct';
+        color = rightResultColor;
         numPoints += 1;
         newQuest(jsonData);
     } else if (numTries < 1) {
-        text = 'It was ' + method + '!\nMaybe this one?';
+        text = 'Wrong.';
+        answer = 'It was<i> ' + method + '</i>!\nMaybe this one?';
+        color = wrongResultColor;
         newQuest(jsonData);
     } else {
         numTries -= 1;
         text = 'Wrong, try again.';
     }
-    timeoutResult();
-    document.getElementById('count').innerHTML = numPoints + '/' + (numQuests - 1);
-    document.getElementById('result').innerHTML = text;
+    printResult(text, answer, color);
 };
 
 var hint = function() {
     var text;
+    var color = 'white';
+    var answer = '';
     if (isCorrect()) {
         text = 'Correct';
+        color = rightResultColor;
         numPoints += 1;
         newQuest(jsonData);
     } else {
@@ -103,15 +144,21 @@ var hint = function() {
         text = 'Come on, now you\'ll know!';
         document.getElementById('guessInput').value = method.substring(0, Math.floor(method.length / 2));
     }
-    timeoutResult();
-    document.getElementById('count').innerHTML = numPoints + '/' + (numQuests - 1);
-    document.getElementById('result').innerHTML = text;
+    printResult(text, answer, color);
 };
 
+var printResult = function(text, answer, color) {
+    document.getElementById('guessInput').value == method;
+    document.getElementById('count').innerHTML = numPoints + '/' + (numQuests - 1);
+    document.getElementById('flag').innerHTML = text;
+    document.getElementById('flag').style.background = color;
+    document.getElementById('answer').innerHTML = answer;
+    setTimeout('blankResult()', 5000);
+
+}
+
 document.addEventListener('keypress', function(e) {
-    if (e.keyCode === 13 && isCorrect()) {
-        check();
-    } else if (e.keyCode === 13) {
+    if (e.keyCode === 13) {
         check();
     }
 });
