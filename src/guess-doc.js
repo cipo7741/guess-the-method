98b7042b2c13bf0896d42108117bf533f6fcd9d4
cipo7741 <a@b.c>
2020-02-23 22:44:04 +0100

    prototype of awesome new sidebar

---
+++ src/guess-doc.js
@@ -0,0 +1,99 @@
+'use strict'
+
+var resourcesPath = 'data/java/java.json';
+var jsonData;
+
+var loadJson = function(){
+    return new Promise(
+        function(resolve, reject) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('GET', resourcesPath, true);
+            xmlhttp.onload = function() {
+                if (xmlhttp.status === 200) {
+                    jsonData = JSON.parse(xmlhttp.responseText);
+                    console.log(jsonData.length);
+                    loadSidebar(jsonData);
+                }
+            }
+            xmlhttp.onerror = function() {
+                Error('Data didn\'t load successfully; error code:' + request.statusText);
+            }
+            xmlhttp.send();
+        }
+        );
+}
+
+loadJson();
+
+var loadSidebar = function(jsonData) {
+    console.log(jsonData.packages);
+    loadPackages(jsonData.packages);
+}
+
+
+//         <label>
+//             <input type="checkbox" checked="checked">
+//           <span class="checkmark"></span>
+//           <p>One</p>
+//           <p>0/1</p>
+//         </label>
+//         <label>
+//             <input type="checkbox" checked="checked">
+//           <span class="checkmark"></span>
+//           <p>One</p>
+//           <p>0/1</p>
+//         </label>
+//         <label>
+//             <input type="checkbox" checked="checked">
+//           <span class="checkmark"></span>
+//           <p>One</p>
+//           <p>0/1</p>
+//         </label>
+//         <label>
+//             <input type="checkbox" checked="checked">
+//           <span class="checkmark"></span>
+//           <p>One</p>
+//           <p>0/1</p>
+//         </label>
+
+var loadPackages = function(jsonPackagesArray) {
+    for (var i = 0; i < jsonPackagesArray.length; i++) {
+        var label = document.createElement('label');
+        var input = document.createElement('input');    
+        input.setAttribute('type', 'checkbox');
+        var inputXml = new XMLSerializer().serializeToString(input);
+        label.innerHTML = inputXml;
+        var pText = document.createElement('p');
+        var pTextCount = document.createElement('p');
+        pText.innerHTML = jsonPackagesArray[i].name;
+        var pTextXml = new XMLSerializer().serializeToString(pText);
+        label.innerHTML += pTextXml;
+        
+        loadClasses(jsonPackagesArray[i].classes);
+        
+        pTextCount.innerHTML = '0/' + jsonPackagesArray[i].classes.length;
+        var pTextCountXml = new XMLSerializer().serializeToString(pTextCount);
+        label.innerHTML += pTextCountXml;
+        
+        var labelXml = new XMLSerializer().serializeToString(label);
+        document.getElementById('packages').innerHTML += labelXml;
+    }
+}
+
+var loadClasses = function(jsonClassesArray) {
+    for (var i = 0; i < jsonClassesArray.length; i++) {
+        var label = document.createElement('label');
+        var input = document.createElement('input');    
+        input.setAttribute('type', 'checkbox');
+        var inputXml = new XMLSerializer().serializeToString(input);
+        label.innerHTML = inputXml;
+        var pText = document.createElement('p');
+        pText.innerHTML = jsonClassesArray[i];
+        var pTextXml = new XMLSerializer().serializeToString(pText);
+        label.innerHTML += pTextXml;
+
+        var labelXml = new XMLSerializer().serializeToString(label);
+        document.getElementById('classes').innerHTML += labelXml;
+    }
+}
+
