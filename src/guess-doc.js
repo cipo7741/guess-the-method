652de4f68b41a3de2900e8460499cd570721b26e
cipo7741 <cipo7741@users.noreply.github.com>
2020-03-14 18:43:04 +0100

    adds different ui/ux

--- src/guess-doc.js
+++ src/guess-doc.js
@@ -1,8 +1,6 @@
 'use strict'
-
 var resourcesPath = 'data/java/java.json';
 var jsonData;
-
 var loadJson = function(){
     return new Promise(
         function(resolve, reject) {
@@ -22,15 +20,11 @@ var loadJson = function(){
         }
         );
 }
-
 loadJson();
-
 var loadSidebar = function(jsonData) {
     console.log(jsonData.packages);
     loadPackages(jsonData.packages);
 }
-
-
 //         <label>
 //             <input type="checkbox" checked="checked">
 //           <span class="checkmark"></span>
@@ -55,11 +49,10 @@ var loadSidebar = function(jsonData) {
 //           <p>One</p>
 //           <p>0/1</p>
 //         </label>
-
 var loadPackages = function(jsonPackagesArray) {
     for (var i = 0; i < jsonPackagesArray.length; i++) {
         var label = document.createElement('label');
-        var input = document.createElement('input');    
+        var input = document.createElement('input');
         input.setAttribute('type', 'checkbox');
         var inputXml = new XMLSerializer().serializeToString(input);
         label.innerHTML = inputXml;
@@ -68,22 +61,22 @@ var loadPackages = function(jsonPackagesArray) {
         pText.innerHTML = jsonPackagesArray[i].name;
         var pTextXml = new XMLSerializer().serializeToString(pText);
         label.innerHTML += pTextXml;
-        
-        loadClasses(jsonPackagesArray[i].classes);
-        
         pTextCount.innerHTML = '0/' + jsonPackagesArray[i].classes.length;
         var pTextCountXml = new XMLSerializer().serializeToString(pTextCount);
         label.innerHTML += pTextCountXml;
-        
         var labelXml = new XMLSerializer().serializeToString(label);
         document.getElementById('packages').innerHTML += labelXml;
+        var classesDivXml = loadClasses(jsonPackagesArray[i].classes);
+        document.getElementById('packages').innerHTML += classesDivXml;
     }
 }
-
 var loadClasses = function(jsonClassesArray) {
+    var classesDiv = document.createElement('div');
+    classesDiv.setAttribute('class','sub-checkboxes');
+
     for (var i = 0; i < jsonClassesArray.length; i++) {
         var label = document.createElement('label');
-        var input = document.createElement('input');    
+        var input = document.createElement('input');
         input.setAttribute('type', 'checkbox');
         var inputXml = new XMLSerializer().serializeToString(input);
         label.innerHTML = inputXml;
@@ -91,9 +84,8 @@ var loadClasses = function(jsonClassesArray) {
         pText.innerHTML = jsonClassesArray[i];
         var pTextXml = new XMLSerializer().serializeToString(pText);
         label.innerHTML += pTextXml;
-
         var labelXml = new XMLSerializer().serializeToString(label);
-        document.getElementById('classes').innerHTML += labelXml;
+        classesDiv.innerHTML += labelXml;
     }
+    return new XMLSerializer().serializeToString(classesDiv);
 }
-
